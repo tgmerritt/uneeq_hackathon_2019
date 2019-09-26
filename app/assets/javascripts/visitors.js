c3cc5a2e635e70c1d1426e4ef2b59052f6a27482
Tyler Merritt <tgmerritt@gmail.com>
2019-09-25 22:23:45 -0500

    Fix the trigger on videos

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
         $(".uneeq-modal-btn").show();
         $("#video-intro").hide();
         victor_video.show();
+        victor_video.get(0).play();
         $("#up-button").show();
         $("#down-button").show();
     });
@@ -17,22 +18,31 @@ $(document).ready(function () {
     $("#up-button").click(function () {
         if (victor_video.is(":visible")) {
             victor_video.hide();
+            victor_video.get(0).pause();
+            victor_video.get(0).currentTime = 0;
             alyssa_video.hide();
             sophie_video.show();
+            sophie_video.get(0).play();
             uneeq_modal_btn.html("About Sophie");
             exampleModalLabel.html("About Sophie");
             modal_body.html("Hi - I'm Sophie.  I've been chewing through investment advice since, like, yesterday.  You can totally trust me with your money.  Just let me know what you want me to buy.");
         } else if (alyssa_video.is(":visible")) {
             alyssa_video.hide();
+            alyssa_video.get(0).pause();
+            alyssa_video.get(0).currentTime = 0;
             sophie_video.hide();
             victor_video.show();
+            victor_video.get(0).play();
             uneeq_modal_btn.html("About Victor");
             exampleModalLabel.html("About Victor");
             modal_body.html("Hi - I'm Victor.  Dashing, Mysterious, and ready to answer your questions about variable-rate mortgages - they've gotten better since the last economic meltdown!");
         } else if (sophie_video.is(":visible")) {
             sophie_video.hide();
+            sophie_video.get(0).pause();
+            sophie_video.get(0).currentTime = 0;
             victor_video.hide();
             alyssa_video.show();
+            alyssa_video.get(0).play();
             uneeq_modal_btn.html("About Alyssa");
             exampleModalLabel.html("About Alyssa");
             modal_body.html("Hi - I'm Alyssa.  I'm completely make-believe, but you can certainly believe that the rates I disclose to you on our call are annual percentage rates.  Let's get started!");
@@ -42,22 +52,31 @@ $(document).ready(function () {
     $("#down-button").click(function () {
         if (victor_video.is(":visible")) {
             victor_video.hide();
-            sophie_video.hide(); // This shouldn't matter, but for posterity
+            victor_video.get(0).pause();
+            victor_video.get(0).currentTime = 0;
+            sophie_video.hide(); 
             alyssa_video.show();
+            alyssa_video.get(0).play();
             uneeq_modal_btn.html("About Alyssa");
             exampleModalLabel.html("About Alyssa");
             modal_body.html("Hi - I'm Alyssa.  I'm completely make-believe, but you can certainly believe that the rates I disclose to you on our call are annual percentage rates.  Let's get started!");
         } else if (alyssa_video.is(":visible")) {
             alyssa_video.hide();
+            alyssa_video.get(0).pause();
+            alyssa_video.get(0).currentTime = 0;
             victor_video.hide(); // This shouldn't matter, but for posterity
             sophie_video.show();
+            sophie_video.get(0).play();
             uneeq_modal_btn.html("About Sophie");
             exampleModalLabel.html("About Sophie");
             modal_body.html("Hi - I'm Sophie.  I've been chewing through investment advice since, like, yesterday.  You can totally trust me with your money.  Just let me know what you want me to buy.");
         } else if (sophie_video.is(":visible")) {
             sophie_video.hide();
+            sophie_video.get(0).pause();
+            sophie_video.get(0).currentTime = 0;
             alyssa_video.hide(); // This shouldn't matter, but for posterity
             victor_video.show();
+            victor_video.get(0).play();
             uneeq_modal_btn.html("About Victor");
             exampleModalLabel.html("About Victor");
             modal_body.html("Hi - I'm Victor.  Dashing, Mysterious, and ready to answer your questions about variable-rate mortgages - they've gotten better since the last economic meltdown!");
