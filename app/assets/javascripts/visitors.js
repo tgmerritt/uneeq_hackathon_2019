62570fc8f75897b9b0ff7fa63a4993ac6b7e8b2b
Tyler Merritt <tgmerritt@gmail.com>
2019-09-25 22:39:11 -0500

    Add a link to the Watson instance of Sophie

--- app/assets/javascripts/visitors.js
+++ app/assets/javascripts/visitors.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-    const victor_video = $("#video-victor");
-    const sophie_video = $("#video-sophie");
-    const alyssa_video = $("#video-alyssa");
-    const uneeq_modal_btn = $(".uneeq-modal-btn");
-    const exampleModalLabel = $("#exampleModalLabel");
-    const modal_body = $(".modal-body");
+    var victor_video = $("#video-victor");
+    var sophie_video = $("#video-sophie");
+    var alyssa_video = $("#video-alyssa");
+    var uneeq_modal_btn = $(".uneeq-modal-btn");
+    var exampleModalLabel = $("#exampleModalLabel");
+    var modal_body = $(".modal-body");
+    var select_button = $("#select-button");
 
     $("#explore-button").click(function () {
         $(".uneeq-modal-btn").show();
@@ -23,6 +24,7 @@ $(document).ready(function () {
             alyssa_video.hide();
             sophie_video.show();
             sophie_video.get(0).play();
+            select_button.show();
             uneeq_modal_btn.html("About Sophie");
             exampleModalLabel.html("About Sophie");
             modal_body.html("Hi - I'm Sophie.  I've been chewing through investment advice since, like, yesterday.  You can totally trust me with your money.  Just let me know what you want me to buy.");
@@ -31,6 +33,7 @@ $(document).ready(function () {
             alyssa_video.get(0).pause();
             alyssa_video.get(0).currentTime = 0;
             sophie_video.hide();
+            select_button.hide();
             victor_video.show();
             victor_video.get(0).play();
             uneeq_modal_btn.html("About Victor");
@@ -38,6 +41,7 @@ $(document).ready(function () {
             modal_body.html("Hi - I'm Victor.  Dashing, Mysterious, and ready to answer your questions about variable-rate mortgages - they've gotten better since the last economic meltdown!");
         } else if (sophie_video.is(":visible")) {
             sophie_video.hide();
+            select_button.hide();
             sophie_video.get(0).pause();
             sophie_video.get(0).currentTime = 0;
             victor_video.hide();
@@ -54,7 +58,8 @@ $(document).ready(function () {
             victor_video.hide();
             victor_video.get(0).pause();
             victor_video.get(0).currentTime = 0;
-            sophie_video.hide(); 
+            sophie_video.hide();
+            select_button.hide();
             alyssa_video.show();
             alyssa_video.get(0).play();
             uneeq_modal_btn.html("About Alyssa");
@@ -66,12 +71,14 @@ $(document).ready(function () {
             alyssa_video.get(0).currentTime = 0;
             victor_video.hide(); // This shouldn't matter, but for posterity
             sophie_video.show();
+            select_button.show();
             sophie_video.get(0).play();
             uneeq_modal_btn.html("About Sophie");
             exampleModalLabel.html("About Sophie");
             modal_body.html("Hi - I'm Sophie.  I've been chewing through investment advice since, like, yesterday.  You can totally trust me with your money.  Just let me know what you want me to buy.");
         } else if (sophie_video.is(":visible")) {
             sophie_video.hide();
+            select_button.hide();
             sophie_video.get(0).pause();
             sophie_video.get(0).currentTime = 0;
             alyssa_video.hide(); // This shouldn't matter, but for posterity
